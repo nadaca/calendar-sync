7433ffee5788b0bbad1d83f9c7ed01015dbb66ff
nadaca <36363394+nadaca@users.noreply.github.com>
2025-04-23 13:24:21 +0000

    trying to make to oauth working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import streamlit_google_oauth as oauth
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+client_id = st.secrets["GOOGLE_CLIENT_ID"]
+client_secret = st.secrets["GOOGLE_CLIENT_SECRET"]
+redirect_uri = st.secrets["GOOGLE_REDIRECT_URI"]
+
+login_info = oauth.login(
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri=redirect_uri,
+        logout_button_text="Logout",
+    )
+if login_info:
+    user_id, user_email = login_info
+    st.write(f"Welcome {user_email}")
